8cb543dbe6a098cc3b2ca365bfa341d5d4e6b266
Alan Suarez <alansuarez@Alans-MBP.lan>
2022-03-21 14:07:52 -0700

    added landing page html and css

---
+++ public/main.js
@@ -0,0 +1,7 @@
+const menuToggle = document.querySelector(".toggle");
+const homepage = document.querySelector(".homepage");
+
+menuToggle.addEventListener("click", () => {
+  menuToggle.classList.toggle("active");
+  homepage.classList.toggle("active");
+});
